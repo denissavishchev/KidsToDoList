b9a76a8f0d3b6256e80d27a6927c7c173c9049f2
Denis Savishchev <denis.savishchev@gmail.com>
2025-01-19 09:00:47 +0100

    Initial Commit

---
+++ KidsToDoList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KidsToDoList
+//
+//  Created by Devis on 19/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KidsToDoList/KidsToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidsToDoListApp.swift
+//  KidsToDoList
+//
+//  Created by Devis on 19/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct KidsToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
